bc6b88acc85754f4836cbaf686e5c28a7e66defe
Monica <monica_emediato@hotmail.com>
2018-08-03 14:51:32 -0300

    Commit 03/08

---
+++ WebApresentation/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApresentation.Models;
+
+namespace WebApresentation.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+			var model = new List<ClienteModel>();
+
+			model.Add(new ClienteModel()
+			{
+				nomeCliente = "Monica",
+				rgCliente = 197828,
+				enderecoCliente = "Rua Flavio",
+				bairroCliente = "B",
+				cidadeCliente = "Belo Horizonte",
+				estadoCliente = "MG",
+				CEPCliente = "30494030",
+			//	nascimentoCliente = 20130816
+			});
+            return View(model);
+        }
+
+		public ActionResult CadView(int? idCliente)
+		{
+			try
+			{
+				if (idCliente == null)
+				{
+					return View();
+				}
+				else
+				{
+					var model = new ClienteModel()
+						{
+							nomeCliente = "Monica",
+							rgCliente = 197828,
+							enderecoCliente = "Rua Flavio",
+							bairroCliente = "B",
+							cidadeCliente = "Belo Horizonte",
+							estadoCliente = "MG",
+							CEPCliente = "30494030",
+						};
+					return View(model);
+				}
+			}
+			catch(Exception)
+		}
+	}
+}
---
+++ WebApresentation/Models/ClienteModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApresentation.Models
+{
+	public class ClienteModel
+	{
+		public int codCliente { get; protected set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[Display(Name ="Nome")]
+		public string nomeCliente { get; set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[Display(Name = "Rg")]
+		[MaxLength(9, ErrorMessage = "Campo com tamanho máximo de 9 caracteres!")]
+		public double rgCliente { get; set; }
+
+		[Display(Name = "Endereco")]
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		public string enderecoCliente { get; set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[Display(Name = "Bairro")]
+		public string bairroCliente { get; set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[Display(Name = "Cidade")]
+		public string cidadeCliente { get; set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[Display(Name = "Estado")]
+		public string estadoCliente { get; set; }
+
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		[MaxLength(7, ErrorMessage = "Campo com tamanho máximo de 7 caracteres!")]
+		[Display(Name = "CEP")]
+		public string CEPCliente { get; set; }
+
+		[Display(Name = "Data Nascimento")]
+		[Required(ErrorMessage = "Campo Obrigatório!")]
+		public DateTime nascimentoCliente { get; set; }
+
+
+		//cliente model view para adicionar o tipo para somente o usuario ver
+	}
+}
